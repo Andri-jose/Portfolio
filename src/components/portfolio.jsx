cd939a8eaef42b478eb1f8394ee3e486945c0992
Andri Salazar <andr-y2008@hotmail.com>
2026-02-16 17:28:38 -0300

    save

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -8,19 +8,6 @@ function Portfolio() {
         <h1 className="text-[#4FA1C6]">These are some of my projects</h1>
 
         <div className="md:grid md:grid-cols-3 mt-15 flex flex-col">
-          <div className="bg-black flex-col flex justify-evenly items-center px-5 py-18 font-medium leading-6">
-            <h2 className="text-lg font-semibold pb-5 md:pb-0">Real Estate Landing Page</h2>
-            <p className="pb-5 md:pb-0">
-              Landing page created for a real estate company, created with React, TypeScript, Tailwind CSS, useState, Vitest for Testing.
-            </p>
-            <button onClick={() => window.open('https://andri-jose.github.io/Landing-Page-Real-Estate/', '_blank')}>
-              View more
-            </button>
-          </div>
-          <img src="./IMG/image-realestate.png" className="col-span-2 w-full h-full " alt="Description" />
-        </div>
-
-        <div className="md:grid md:grid-cols-3 mt-15 flex flex-col">
           <img src="./IMG/Worldwide.avif" className="col-span-2 w-full h-full" alt="Description" />
           <div className="bg-black flex-col flex justify-evenly items-center px-5 py-18  font-medium leading-6">
             <h2 className="text-lg font-semibold pb-5 md:pb-0">WorldWise App</h2>
@@ -47,6 +34,18 @@ function Portfolio() {
           </div>
         </div>
 
+        <div className="md:grid md:grid-cols-3 mt-15 flex flex-col">
+          <div className="bg-black flex-col flex justify-evenly items-center px-5 py-18 font-medium leading-6">
+            <h2 className="text-lg font-semibold pb-5 md:pb-0">Real Estate Landing Page</h2>
+            <p className="pb-5 md:pb-0">
+              Landing page created for a real estate company, created with React, TypeScript, Tailwind CSS, useState, Vitest for Testing.
+            </p>
+            <button onClick={() => window.open('https://andri-jose.github.io/Landing-Page-Real-Estate/', '_blank')}>
+              View more
+            </button>
+          </div>
+          <img src="./IMG/image-realestate.png" className="col-span-2 w-full h-full " alt="Description" />
+        </div>
      
       </section>
 
